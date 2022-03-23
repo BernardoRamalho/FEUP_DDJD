b1839ae4243c84ff6926a18ffe94cf6c91dacfa2
BernardoRamalho <bernardoramalho99@gmail.com>
2022-03-23 17:53:54 +0000

    Bug enemy moves to the correct place on the scene

---
+++ Assets/Scripts/BugAI.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugAI : MonoBehaviour
+{
+    private PlayerController2D character;
+
+    public float maxHealth = 50.0f;
+    public float health;
+    public float acceleration = 3.0f;
+    public Rigidbody2D rb;
+    public int chargingTime = 100;
+    public float scaleFactor = 0.1f;
+    private Vector2 screenBounds;
+
+    [HideInInspector] private int timer = 0;
+    [HideInInspector] private bool isCharging = false;
+
+    private enum State {
+        Moving,
+        Growing,
+        Attacking
+    }
+
+    private State state = State.Moving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        rb = this.GetComponent<Rigidbody2D>();
+        character = FindObjectOfType<PlayerController2D>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,Camera.main.transform.position.z));
+        Debug.Log(screenBounds);
+        Debug.Log(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        switch(state){
+            case State.Moving:
+                Move();
+                break;
+            case State.Growing:
+                Grow();
+                break;
+            case State.Attacking:
+                Attack();
+                break;
+        }     
+        
+    }
+
+    void Move(){
+        float x = 0.0f;
+
+        if(transform.position.x > (screenBounds.x- screenBounds.x/3)){
+            Debug.Log(transform.position.x);
+            Debug.Log(screenBounds.x/3);
+            x = -acceleration;   
+        }
+
+        if(x == 0.0f){
+            state = State.Growing;
+        }
+ 
+        rb.velocity = new Vector2(x, 0.0f);
+        
+    }
+
+    void Grow(){
+
+    }
+
+    void Attack(){
+
+    }
+}
+
