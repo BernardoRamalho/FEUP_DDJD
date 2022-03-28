d61fa107b0e37da42087a8456e8d45538963a5e3
BernardoRamalho <bernardoramalho99@gmail.com>
2022-03-28 14:23:49 +0100

    Added Enemy Abstract Class

--- Assets/Scripts/ProfessorAI.cs
+++ Assets/Scripts/ProfessorAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProfessorAI : MonoBehaviour
+public class ProfessorAI : Enemy
 {
 
     private PlayerController2D character;
