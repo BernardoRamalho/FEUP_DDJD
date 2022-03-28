7c5905a15617440a3c503205be62482decd98089
BernardoRamalho <bernardoramalho99@gmail.com>
2022-03-28 14:00:30 +0100

    Added PowerUp abstract class

--- Assets/Scripts/Coffee.cs
+++ Assets/Scripts/Coffee.cs
@@ -2,24 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coffee : MonoBehaviour
+public class Coffee : PowerUp
 {
-    public float speed = 10.0f;
-    private Rigidbody2D rb;
-    private Vector2 screenBounds;
-
     public float normalShootingDelay;
     public float improvedShootingDelay;
     public float effectDuration;
     public bool effectActive = false;
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-speed, 0);
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,Camera.main.transform.position.z));
-    }
 
     // Update is called once per frame
     void Update()
@@ -38,7 +26,7 @@ public class Coffee : MonoBehaviour
         }
     }
 
-    private void StartPowerUp(GameObject playerGameObject) {
+    protected override void StartPowerUp(GameObject playerGameObject) {
         MovementScript script = playerGameObject.GetComponent<MovementScript>();
         script.shootingDelay = improvedShootingDelay;
         effectActive = true;
--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -2,20 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shield : MonoBehaviour
+public class Shield : PowerUp
 {
-    public float speed = 10.0f;
-    private Rigidbody2D rb;
-    private Vector2 screenBounds;
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-speed, 0);
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,Camera.main.transform.position.z));
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -32,9 +20,9 @@ public class Shield : MonoBehaviour
         }
     }
 
-    private void StartPowerUp(GameObject playerGameObject) {
+    protected override void StartPowerUp(GameObject playerGameObject) {
         PlayerController2D script = playerGameObject.GetComponent<PlayerController2D>();
-        
+
         script.activateShield();
     }
 }
