c9426f8d7d72a983d45962b5222aaa97cb1c4d7a
BernardoRamalho <bernardoramalho99@gmail.com>
2022-04-06 18:03:01 +0100

    Added Exit Button

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -7,14 +7,22 @@ public class ButtonHandler : MonoBehaviour
 {
 
 	public UnityEngine.UI.Button startBtn;
+	public UnityEngine.UI.Button exitBtn;
 
 	void Start()
 	{
 		startBtn.onClick.AddListener(OnClickStart);
+		exitBtn.onClick.AddListener(OnClickExit);
+		
 	}
 
 	void OnClickStart()
 	{
 		SceneManager.LoadScene("GameScene");
+	}
+
+	void OnClickExit()
+	{
+		Application.Quit();
 	}
 }
